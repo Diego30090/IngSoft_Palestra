391865e9723affb8b866ce498e6b144d06287756
Diego30090 <diego.vaccarini@gmail.com>
2022-08-20 12:30:00 +0200

    definita la request per le info utente, con il tipo di user

--- db/dbController.py
+++ db/dbController.py
@@ -21,57 +21,16 @@ def login(user, pwd):
     else:
         return False
 
+def user_info(user):
+    cur = connect()
+    query = f"SELECT * FROM utente WHERE username='{user}';"
+    val = cur.execute(query)
+    val= val.fetchall()
+    return val[0]
 
-def menu(con, cur):
-    value = input(
-        'Welcome to the menu.\nWhat do you want to do? \n     1)Check the schema\n     2)Select a table\n     '
-        '3)Delete a record\n')
-    if value == '1':
-        check_schema(con, cur)
-    elif value == '2':
-        exec_select(con, cur)
-    elif value == '3':
-        del_elem(con, cur)
-    else:
-        print('insert correct value\n')
-        menu(con, cur)
-
-
-def check_schema(con, cur):
-    schema = cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    info_schema = cur.fetchall()
-    print(str(info_schema))
-    menu(con, cur)
-
-
-def exec_select(con, cur):
-    sel = input('from which table do you want to select?\n')
-    print(f'chosen table {sel}\n')
-    rows = cur.execute(f'SELECT * FROM {sel}')
-    for row in rows:
-        print(row)
-    print('\n')
-    menu(con, cur)
-
-
-def del_elem(con, cur):
-    table = input('from which table do you want to delete?\n')
-    record = input('Which record do you want to delete?\n')
-    cur.execute(f'DELETE FROM {table} WHERE id={record}')
-    menu(con, cur)
-
-
-def task_insert(val: int, table: str):
-    con = sqlite3.connect('dbProject.db')
-    cur = con.cursor()
-    if val == 1:
-        query = f'SELECT * FROM {table}'
-
+def user_type(user):
+    val = user_info(user)
+    return val[12]
 
 if __name__ == "__main__":
-    '''
-    con = sqlite3.connect('dbProject.db')
-    cur = con.cursor()
-    #menu(con, cur)
-    '''
-
+    uT = user_type('root')
