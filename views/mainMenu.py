17ad9c295509a5ae3994ba33e281a391116d0cab
Diego30090 <diego.vaccarini@gmail.com>
2022-10-04 16:25:16 +0200

    Inserted button colors

--- views/mainMenu.py
+++ views/mainMenu.py
@@ -54,28 +54,30 @@ class MainMenu(QWidget):
             local_buttons[elem].setText(local_buttons_name[elem])
             local_buttons[elem].resize(*btn_dim)
         btn_row1 = [self.calendar_button, self.inventory_button, self.profile_button]
+        btn_row1_rgb = ['200,200,200', '200,200,200', '200,200,200']
         btn_row2 = [self.personnelManagement_button, self.logout_button]
+        btn_row2_rgb = ['200,200,200', '255,70,70']
+        self.positioning(btn_row1, self.starting_distance, btn_row1_rgb)
 
-        self.positioning(btn_row1, self.starting_distance)
-
-        self.positioning(btn_row2, self.starting_distance + btn_dim[1] + 30)
+        self.positioning(btn_row2, self.starting_distance + btn_dim[1] + 30, btn_row2_rgb)
 
         self.setFixedWidth(self.width)
         self.setFixedHeight(btn_row2[0].y() + btn_row2[0].height() + self.starting_distance)
 
         self.show()
 
-    def positioning(self, element, selected_height):
+    def positioning(self, element, selected_height, rgb):
         lenght_sum = 0
         for elem in range(len(element)):
             if elem ==0:
                 lenght_sum +=50
             element[elem].move(lenght_sum, selected_height)
             lenght_sum +=element[elem].width() + 30
+            element[elem].setStyleSheet(f"background: rgb({rgb[elem]});")
         if lenght_sum +20 > self.width:
             self.width = lenght_sum +20
 
-        print(lenght_sum)
+
     def control_check(self):
         self.buttons = [self.calendar_button, self.personnelManagement_button, self.profile_button, self.inventory_button]
         atleta_opt = [False, True, False, False]
@@ -91,7 +93,7 @@ class MainMenu(QWidget):
         elif self.user_type == "Istruttore":
             self.disabler(buttons=self.buttons, opt=istruttore_opt)
             self.personnelManagement_button.hide()
-            self.market_button.hide()
+            #self.market_button.hide()
         elif self.user_type == "Admin":
             self.disabler(buttons=self.buttons, opt=admin_opt)
         else:
@@ -99,7 +101,7 @@ class MainMenu(QWidget):
             self.calendar_button.hide()
             self.personnelManagement_button.hide()
             self.profile_button.hide()
-            self.market_button.hide()
+            #self.market_button.hide()
             self.inventory_button.hide()
 
     def disabler(self, buttons, opt):
