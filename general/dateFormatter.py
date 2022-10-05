da74d6b8c19ea0418c0984a3f17d95113fa98820
Diego30090 <diego.vaccarini@gmail.com>
2022-10-05 14:28:42 +0200

    Deleted main from the file

--- general/dateFormatter.py
+++ general/dateFormatter.py
@@ -5,9 +5,3 @@ def str_to_date(val):
     print('returned a date')
     date=val.split('-')
     return QDate(int(date[0]), int(date[1]), int(date[2]))
-
-
-if __name__ == '__main__':
-    val='1955-02-21'
-    d= QDate(9,9,9)
-    print(str(str_to_date(val)))
