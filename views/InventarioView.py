ccbb21a45ff502a8ff54f9d9a684cbdece97648b
RafNH <rafnacca00@gmail.com>
2022-10-08 15:48:06 +0200

    rinominati vari attributi e operazioni

--- views/InventarioView.py
+++ views/InventarioView.py
@@ -7,7 +7,7 @@ from db import dbController as db
 from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QWidget, QTableWidget, QTableWidgetItem, \
                             QComboBox
 
-class inventarioView(QWidget):
+class InventarioView(QWidget):
 
     def __init__(self, username):
         self.username = username
@@ -30,7 +30,7 @@ class inventarioView(QWidget):
         self.button_horizontal_distance = 265
         self.button_vertical_distance = 40
         # Table columns settings
-        self.table_column = None
+        self.table_columns = None
         self.table_fixed_width = 690
         self.table_fixed_height = 400
         # settings for new windows
@@ -38,11 +38,11 @@ class inventarioView(QWidget):
         self.insert_window = None
         self.table_on = False
         # item initializers and item commands
-        self.inventario_UI()
+        self.inventarioUI()
         self.instruction()
 
 
-    def inventario_UI(self):
+    def inventarioUI(self):
         self.setWindowTitle(self.title)
         self.setFixedWidth(self.width)
         self.setFixedHeight(self.height)
@@ -79,44 +79,44 @@ class inventarioView(QWidget):
         self.create_button.move(100, 580)
         self.create_button.setStyleSheet("background: rgb(140,255,70);")
 
-        self.modify_button = QPushButton('Modifica', self)
+        self.modify_button = QPushButton('Visualizza', self)
         self.modify_button.setFixedHeight(self.button_fixed_height)
         self.modify_button.setFixedWidth(self.button_fixed_width)
         self.modify_button.move(self.armi_button.x() + self.button_horizontal_distance, 580)
         self.modify_button.setStyleSheet("background: rgb(255,255,70);")
 
     def instruction(self):
-        self.armi_button.clicked.connect(lambda: self.show_tab('armi'))
-        self.divise_button.clicked.connect(lambda: self.show_tab('divise'))
-        self.borsoni_button.clicked.connect(lambda: self.show_tab('borsoni'))
-        self.create_button.clicked.connect(lambda: self.insMod(False))
+        self.armi_button.clicked.connect(lambda: self.showTab('armi'))
+        self.divise_button.clicked.connect(lambda: self.showTab('divise'))
+        self.borsoni_button.clicked.connect(lambda: self.showTab('borsoni'))
+        self.create_button.clicked.connect(lambda: self.insertModify(False))
         if self.table_on is True:
             self.display_table.cellClicked.connect(lambda: self.selectRow)
         self.back_button.clicked.connect(self.toMainMenu)
 
-    def show_tab(self, tab_type):
+    def showTab(self, tab_type):
         self.table_on = True
 
         if tab_type == 'armi':
-            self.table_column = ['Giacenza', 'Disponibilità', 'Arma', 'D/S', 'Materiale', 'Lunghezza', 'Produttore',
+            self.table_columns = ['Giacenza', 'Disponibilità', 'Arma', 'D/S', 'Materiale', 'Lunghezza', 'Produttore',
                                  'Impugnatura']
         elif tab_type == 'divise':
-            self.table_column = ['Giacenza', 'Disponibilità', 'Elemento', 'D/S', 'Arma', 'Taglia', 'Sesso',
+            self.table_columns = ['Giacenza', 'Disponibilità', 'Elemento', 'D/S', 'Arma', 'Taglia', 'Sesso',
                                  'Produttore']
         elif tab_type == 'borsoni':
-            self.table_column = ['Giacenza', 'Disponibilità', 'Attrezzatura', 'Produttore']
+            self.table_columns = ['Giacenza', 'Disponibilità', 'Attrezzatura', 'Produttore']
         self.tab_name = tab_type
         lista = db.select_inventario(tab_type)
-        self.display_table = QTableWidget(len(lista), len(self.table_column), self)
+        self.display_table = QTableWidget(len(lista), len(self.table_columns), self)
         self.display_table.move(80, 150)
         self.display_table.setFixedWidth(self.table_fixed_width)
         self.display_table.setFixedHeight(self.table_fixed_height)
-        self.display_table.setHorizontalHeaderLabels(self.table_column)
+        self.display_table.setHorizontalHeaderLabels(self.table_columns)
 
         self.id_list = []
         for i in range(len(lista)):
             id_number = lista[i][0]
-            for j in range(len(self.table_column)):
+            for j in range(len(self.table_columns)):
                 item = QTableWidgetItem(str(lista[i][j+1]))
                 item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
                 self.display_table.setItem(i, j, item)
@@ -126,20 +126,15 @@ class inventarioView(QWidget):
         self.modify_button.show()
         self.display_table.show()
 
-
-
-
     def selectRow(self):
-        print('selectrow partita')
         self.modify_button.show()
         position = self.display_table.currentRow()
         item = db.select_inventario_by_id(self.id_list[position], self.tab_name)
         item.pop(0)
-        print(item)
         return item
 
-    def insMod(self, info):
-        self.insert_window = InventarioCrudView(f'Inserisci {self.tab_name}', self.table_column, self.tab_name, info, self.username)
+    def insertModify(self, info):
+        self.insert_window = InventarioCRUDView(f'Inserisci {self.tab_name}', self.table_columns, self.tab_name, info, self.username)
         self.insert_window.show()
         self.hide()
 
@@ -150,12 +145,12 @@ class inventarioView(QWidget):
 
 
 #INSertMODifyWINDOW
-class InventarioCrudView(QWidget):
+class InventarioCRUDView(QWidget):
 
     def __init__(self, window_title, table_column, tab_name, info, username):
         super().__init__()
         self.item = info
-        self.username=username
+        self.username = username
     # Window settings
         self.title = window_title
         self.width = 370
@@ -175,11 +170,11 @@ class InventarioCrudView(QWidget):
         self.horizontalLabelDistance = 50
         self.horizontalTextDistance = 200
 
-        self.insModUI(table_column)
+        self.insertModifyUI(table_column)
         self.instruction()
         self.show()
 
-    def insModUI(self, table_column):
+    def insertModifyUI(self, table_column):
         self.setWindowTitle(self.title)
         self.setFixedWidth(self.width)
         self.setFixedHeight(self.height)
@@ -242,7 +237,7 @@ class InventarioCrudView(QWidget):
         self.back_button.move(self.horizontalTextDistance, 520)
         self.back_button.setStyleSheet("background: rgb(255,70,70);")
 
-    def save_infos(self):
+    def saveChanges(self):
         info = []
         for i in range(len(self.texts)):
             if isinstance(self.texts[i], QLineEdit):
@@ -250,16 +245,16 @@ class InventarioCrudView(QWidget):
             elif isinstance(self.texts[i], QComboBox):
                 info.append(self.texts[i].currentText())
         db.insert_inventario(self.tab_name, info)
-        self.close_this()
+        self.closeThis()
 
     def instruction(self):
-        self.back_button.clicked.connect(lambda: self.close_this())
-        self.accept_button.clicked.connect(lambda: self.save_infos())
+        self.back_button.clicked.connect(lambda: self.closeThis())
+        self.accept_button.clicked.connect(lambda: self.saveChanges())
 
-    def close_this(self):
-        self.screen = inventarioView(username=self.username)
+    def closeThis(self):
+        self.screen = InventarioView(username=self.username)
         self.screen.show()
-        self.screen.show_tab(tab_type=self.tab_name)
+        self.screen.showTab(tab_type=self.tab_name)
         self.close()
 
 
@@ -270,7 +265,7 @@ if __name__ == '__main__':
     item = np.asarray(item)
     item = item.flatten()
     print(item)'''
-    ex = inventarioView(username='root0')
+    ex = InventarioView(username='root0')
     '''ix = insModWindow('Inserisci arma', ['Giacenza', 'Disponibilità', 'Arma', 'D/S', 'Materiale', 'Lunghezza', 'Produttore',
                                  'Impugnatura'],'armi', item)'''
     sys.exit(app.exec_())
