b82ce0aded81c260e6279925b1359a6f43e45735
Diego30090 <diego.vaccarini@gmail.com>
2022-10-24 18:40:06 +0200

    Inserita funzione di login e definizione account nel db, tramite gestore

--- Controller/GestioneUtente/GestoreAccount.py
+++ Controller/GestioneUtente/GestoreAccount.py
@@ -8,8 +8,11 @@ class GestioneAccount(object):
         self.__cursor = self.__db.cursor()
 
     def login(self):
+        ## Controlla che l'utente desiderato sia presente con username e password indicate
         query = f"SELECT COUNT(id_utente) FROM utente WHERE username = '{self.__utente.getUsername()}' AND password = '{self.__utente.getPassword()}';"
-        value= self.__cursor.execute(query).fetchone()
+        value = self.__cursor.execute(query).fetchone()
+        ## Se esiste, istanzia completamente il model
+        ## Altrimenti, ritorna un valore di false
         if value[0] == 1:
             self.__utente.setInfo(self.getUserInfoInDb('id_utente'), self.getUserInfoInDb('nome'),
                                   self.getUserInfoInDb('cognome'), self.getUserInfoInDb('data_nascita'),
@@ -21,10 +24,19 @@ class GestioneAccount(object):
             return False
 
     def getUserInfoInDb(self, column):
-        query= f"SELECT {column} FROM utente WHERE username = '{self.__utente.getUsername()}' AND password = '{self.__utente.getPassword()}';"
+        query= f"SELECT {column} FROM utente WHERE username = '{self.__utente.getUsername()}' AND " \
+               f"password = '{self.__utente.getPassword()}';"
         val = self.__cursor.execute(query).fetchone()
         return val[0]
 
+    def setUserInfoInDb(self):
+        query = f"INSERT INTO utente(nome, cognome, data_nascita, username, password, utente_tipo, email, telefono) VALUES " \
+                f"('{self.__utente.getNome()}','{self.__utente.getCognome()}'," \
+                f" '{self.__utente.getDataDiNascita()}', '{self.__utente.getUsername()}'," \
+                f" '{self.__utente.getPassword()}', '{self.__utente.getUtenteTipo()}'," \
+                f" '{self.__utente.getEmail()}', '{self.__utente.getTelefono()}') ; "
+        self.__cursor.execute(query)
+        self.__db.commit()
 
 if __name__ == '__main__':
     a= GestioneAccount('root', '0000')
