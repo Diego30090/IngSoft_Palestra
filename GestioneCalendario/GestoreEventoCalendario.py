bbdf9a6212007c179703fc2a45b23a7242b4547d
LorenzoM <S1094728@studenti.univpm.it>
2022-10-24 17:33:34 +0200

    Inizio GestoreEventoCalendario

--- GestioneCalendario/GestoreEventoCalendario.py
+++ GestioneCalendario/GestoreEventoCalendario.py
@@ -1,9 +1,20 @@
+import sqlite3
+
+from models.EventoCalendarioModel import EventoCalendarioModel
 
 class GestoreEventoCalendario():
 
-    def __init__(self):
+    def __init__(self, name_event, date_event, location_event, time_event, organizer_event, description_event):
+        self.evento = EventoCalendarioModel(name_event, date_event, location_event, time_event, organizer_event, description_event)
+        self.db = sqlite3.connect('dbProject.db')
+        self.cursor = self.db.cursor()
+
+    def insertEvent(self):
+        query = f"INSERT INTO tasks(name, date, location, time, organizer, description) VALUES ('{self.name}','{self.date}', '{self.location}','{self.time}', '{self.organizer}', '{self.description}');"
+        self.cursor.execute(query)
+        self.db.commit()
+
         #Deve tenere gli il modello e l'interazione con il db
-        pass
 
     ## Inserire gli algoritmi per l'interazione con il database ed il model
     ## Gli algoritmi creati devono esser utili alla vista
