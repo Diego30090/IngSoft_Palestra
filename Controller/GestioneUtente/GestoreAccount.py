0dc810dd2898d52fb1f70dd7934f01816e5de76a
Diego30090 <diego.vaccarini@gmail.com>
2022-10-31 12:03:37 +0100

    tolta privacy all'utente in GestoreAccount.py

--- Controller/GestioneUtente/GestoreAccount.py
+++ Controller/GestioneUtente/GestoreAccount.py
@@ -3,44 +3,40 @@ import sqlite3
 
 class GestioneAccount(object):
     def __init__(self, username, password):
-        self.__utente = Model(None, None, None, None, username, password, None, None, None)
+        self.utente = Model(None, None, None, None, username, password, None, None, None)
         self.__db = sqlite3.connect('../../db/dbProject.db')
         self.__cursor = self.__db.cursor()
 
-    def getUtenteUsername(self):
-        return self.__utente.getUsername()
 
-    def getUtentePassword(self):
-        return self.__utente.getPassword()
 
     def login(self):
         ## Controlla che l'utente desiderato sia presente con username e password indicate
-        query = f"SELECT COUNT(id_utente) FROM utente WHERE username = '{self.__utente.getUsername()}' AND password = '{self.__utente.getPassword()}';"
+        query = f"SELECT COUNT(id_utente) FROM utente WHERE username = '{self.utente.getUsername()}' AND password = '{self.utente.getPassword()}';"
         value = self.__cursor.execute(query).fetchone()
         ## Se esiste, istanzia completamente il model
         ## Altrimenti, ritorna un valore di false
         if value[0] == 1:
-            self.__utente.setInfo(self.getUserInfoInDb('id_utente'), self.getUserInfoInDb('nome'),
-                                  self.getUserInfoInDb('cognome'), self.getUserInfoInDb('data_nascita'),
-                                  self.getUserInfoInDb('username'), self.getUserInfoInDb('password'),
-                                  self.getUserInfoInDb('utente_tipo'), self.getUserInfoInDb('email'),
-                                  self.getUserInfoInDb('telefono'))
+            self.utente.setInfo(self.getUserInfoInDb('id_utente'), self.getUserInfoInDb('nome'),
+                                self.getUserInfoInDb('cognome'), self.getUserInfoInDb('data_nascita'),
+                                self.getUserInfoInDb('username'), self.getUserInfoInDb('password'),
+                                self.getUserInfoInDb('utente_tipo'), self.getUserInfoInDb('email'),
+                                self.getUserInfoInDb('telefono'))
             return True
         else:
             return False
 
     def getUserInfoInDb(self, column):
-        query= f"SELECT {column} FROM utente WHERE username = '{self.__utente.getUsername()}' AND " \
-               f"password = '{self.__utente.getPassword()}';"
+        query= f"SELECT {column} FROM utente WHERE username = '{self.utente.getUsername()}' AND " \
+               f"password = '{self.utente.getPassword()}';"
         val = self.__cursor.execute(query).fetchone()
         return val[0]
 
     def setUserInfoInDb(self):
         query = f"INSERT INTO utente(nome, cognome, data_nascita, username, password, utente_tipo, email, telefono) VALUES " \
-                f"('{self.__utente.getNome()}','{self.__utente.getCognome()}'," \
-                f" '{self.__utente.getDataDiNascita()}', '{self.__utente.getUsername()}'," \
-                f" '{self.__utente.getPassword()}', '{self.__utente.getUtenteTipo()}'," \
-                f" '{self.__utente.getEmail()}', '{self.__utente.getTelefono()}') ; "
+                f"('{self.utente.getNome()}','{self.utente.getCognome()}'," \
+                f" '{self.utente.getDataDiNascita()}', '{self.utente.getUsername()}'," \
+                f" '{self.utente.getPassword()}', '{self.utente.getUtenteTipo()}'," \
+                f" '{self.utente.getEmail()}', '{self.utente.getTelefono()}') ; "
         self.__cursor.execute(query)
         self.__db.commit()
 
