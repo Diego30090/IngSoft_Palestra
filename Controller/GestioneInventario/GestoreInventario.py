6f37768966a93a0fb1b46d3ab0a6bc6634ed361c
RafNH <rafnacca00@gmail.com>
2022-11-28 18:19:47 +0100

    non ho idea di cosa faccia questo commit

--- Controller/GestioneInventario/GestoreInventario.py
+++ Controller/GestioneInventario/GestoreInventario.py
@@ -1,55 +1,12 @@
+from Model import GestioneInventario
 import sqlite3
 
-from Controller.GestioneDatabase.GestoreDatabase import GestioneDatabase
-from Model.GestioneInventario.DivisaModel import DivisaModel as divisaModel
-from Model.GestioneInventario.ArmaModel import ArmaModel as armaModel
-from Model.GestioneInventario.BorsoneModel import BorsoneModel as borsoneModel
+class GestioneInventario():
 
-
-class GestioneInventario(GestioneDatabase):
     def __init__(self):
-        super().__init__()
-        self.listaDivise: divisaModel = []
-        self.listaArmi: armaModel = []
-        self.listaBorsoni: borsoneModel = []
+        self.listaArmi
+        self.listaDivise
+        self.listaBorsoni
+        self.db = sqlite3.connect('../../db/dbProject.db')
+        self.cursor = self.db.cursor()
 
-        self.getDiviseFromDb()
-        self.getArmiFromDb()
-        self.getBorsoniFromDb()
-
-    def aggiungiDivisa(self, result: divisaModel):
-        self.listaDivise.append(result)
-
-    def aggiungiArma(self, result: armaModel):
-        self.listaArmi.append(result)
-
-    def aggiungiBorsone(self, result: borsoneModel):
-        self.listaBorsoni.append(result)
-
-    def getDiviseFromDb(self):
-        self.listaDivise.clear()
-        diviseList = self.generalizedSelect('divise')
-        for i in range(len(diviseList)):
-            self.aggiungiDivisa(divisaModel(*diviseList[i]))
-
-    def getArmiFromDb(self):
-        self.listaArmi.clear()
-        armiList = self.generalizedSelect('armi')
-        for i in range(len(armiList)):
-            self.aggiungiArma(armaModel(*armiList[i]))
-
-    def getBorsoniFromDb(self):
-        self.listaBorsoni.clear()
-        borsoniList = self.generalizedSelect('borsoni')
-        for i in range(len(borsoniList)):
-            self.aggiungiBorsone(borsoneModel(*borsoniList[i]))
-
-
-if __name__ == '__main__':
-    inv = GestioneInventario()
-    for i in range(len(inv.listaBorsoni)):
-        print(inv.listaBorsoni[i].__dict__)
-    for i in range(len(inv.listaArmi)):
-        print(inv.listaArmi[i].__dict__)
-    for i in range(len(inv.listaDivise)):
-        print(inv.listaDivise[i].__dict__)
