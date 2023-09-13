5f5b98ce2ff6f4c4b393648d4071998dde9a2592
Diego30090 <diego.vaccarini@gmail.com>
2023-09-13 18:32:07 +0200

    Definizione del main effettivo del programma

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication
+from Boundaries.GestioneUtente import loginView as login
 
+if __name__ == '__main__':
+    os.chdir('Boundaries/GestioneUtente')
+    app = QApplication(sys.argv)
+    ex = login.LoginView()
+    sys.exit(app.exec_())
