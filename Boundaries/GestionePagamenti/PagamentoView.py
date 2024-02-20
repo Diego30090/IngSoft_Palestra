4ec0378c5ea98beb303684778f94408cc0d7ef97
Diego30090 <diego.vaccarini@gmail.com>
2024-02-20 17:01:07 +0100

    visualizzazione base PagamentoView.py

--- Boundaries/GestionePagamenti/PagamentoView.py
+++ Boundaries/GestionePagamenti/PagamentoView.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.uic import loadUi
 from Boundaries.GestioneUtente import mainMenu as menu
 from db import dbController as db
 from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QWidget
@@ -7,16 +8,19 @@ from Controller.GestioneUtente.GestoreAccount import GestioneAccount as accountC
 
 class ElencoPagamenti(QWidget):
     def __init__(self, accountController: accountController):
-
+        self.userController = accountController
+        self.username = accountController.utente.getUsername()
+        super(ElencoPagamenti, self).__init__()
+        loadUi("visualizzaPagamenti.ui", self)
         pass
 
     def insertUi(self):
         pass
 
 
-'''
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = ElencoPagamenti(accountController=accountController('root1', 'pwd'))
+    ex.show()
     sys.exit(app.exec_())
-'''
+
